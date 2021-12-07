22255f6864f09eb2d7e272af462e8d5e75cf0125
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2021-12-07 14:11:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,37 +22,14 @@ provider "nutanix" {
   wait_timeout = 60
 }
 
-resource "nutanix_image" "image" {
-  name        = "Arch Linux"
-  description = "Arch-Linux-x86_64-basic-20210401.18564"
-  source_uri  = "https://mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-basic-20210415.20050.qcow2"
+module "karbon" {
+  source                            = "terraform-nutanix-karbon"
+  version                           = "1.0.3"
+  create_karbon                     = true
+  karbon_cluster_name               = "test-karbon-cluster"
+  storage_class_config_username     = var.user
+  storage_class_config_password     = var.password
+  nutanix_cluster_name              = var.cluster_name
+  subnet_name                       = var.subnet_name
 }
 
-resource "nutanix_virtual_machine" "vm" {
-  name                 = "MyVM from the Terraform Nutanix Provider"
-  cluster_uuid         = data.nutanix_cluster.cluster.id
-  num_vcpus_per_socket = "2"
-  num_sockets          = "1"
-  memory_size_mib      = 1024
-
-  disk_list {
-    data_source_reference = {
-      kind = "image"
-      uuid = nutanix_image.image.id
-    }
-  }
-
-  disk_list {
-    disk_size_bytes = 10 * 1024 * 1024 * 1024
-    device_properties {
-      device_type = "DISK"
-      disk_address = {
-        "adapter_type" = "SCSI"
-        "device_index" = "1"
-      }
-    }
-  }
-  nic_list {
-    subnet_uuid = data.nutanix_subnet.subnet.id
-  }
-}
