d507a5a6d000eb2a01fe072113f7b4fb4f86ac13
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2021-12-08 22:27:12 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,29 +7,55 @@ terraform {
   }
 }
 
-data "nutanix_cluster" "cluster" {
-  name = var.cluster_name
-}
-data "nutanix_subnet" "subnet" {
-  subnet_name = var.subnet_name
-}
-
 provider "nutanix" {
-  username     = var.user
-  password     = var.password
-  endpoint     = var.endpoint
-  insecure     = true
-  wait_timeout = 60
+  username = var.user
+  password = var.password
+  endpoint = var.endpoint # prism central endpoint
+  insecure = true
 }
 
-module "karbon" {
-  source                            = "terraform-nutanix-karbon"
-  version                           = "1.0.3"
-  create_karbon                     = true
-  karbon_cluster_name               = "test-karbon-cluster"
-  storage_class_config_username     = var.user
-  storage_class_config_password     = var.password
-  nutanix_cluster_name              = var.cluster_name
-  subnet_name                       = var.subnet_name
-}
+resource "nutanix_karbon_cluster" "exclu2" {
+  name       = "exclu2"
+  version    = "1.20.9-0"
+  storage_class_config {
+    reclaim_policy = "Delete"
+    volumes_config {
+      file_system                = "ext4"
+      flash_mode                 = false
+      password                   = var.password
+      prism_element_cluster_uuid = var.cluuid
+      storage_container          = "Default"
+      username                   = var.user
+    }
+  }
+  cni_config {
+    node_cidr_mask_size = 24
+    pod_ipv4_cidr       = "172.20.0.0/16"
+    service_ipv4_cidr   = "172.19.0.0/16"
+  }
+  worker_node_pool {
+    node_os_version = "ntnx-1.1"
+    num_instances   = 1
+    ahv_config {
+      network_uuid               = var.netuuid
+      prism_element_cluster_uuid = var.cluuid
+    }
+  }
+  etcd_node_pool {
+    node_os_version = "ntnx-1.1"
+    num_instances   = 1
+    ahv_config {
 
+      network_uuid               = var.netuuid
+      prism_element_cluster_uuid = var.cluuid
+    }
+  }
+  master_node_pool {
+    node_os_version = "ntnx-1.1"
+    num_instances   = 1
+    ahv_config {
+      network_uuid               = var.netuuid
+      prism_element_cluster_uuid = var.cluuid
+    }
+  }
+}
